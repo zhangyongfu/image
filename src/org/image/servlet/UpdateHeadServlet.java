a8acc21e65af30a052a1880f35ae9047a4db42a7
zhangyongfu <yffreshstart@gmail.com>
2017-11-26 21:49:49 +0800

    小改动

--- src/org/image/servlet/UpdateHeadServlet.java
+++ src/org/image/servlet/UpdateHeadServlet.java
@@ -1,6 +1,6 @@
 package org.image.servlet;
 
-import org.image.DAO.DatabaseConnection;
+import org.image.DAO.UserPageDao;
 import org.image.model.UserInformation;
 
 import javax.servlet.ServletException;
@@ -9,17 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.File;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 @WebServlet(name = "UpdateHeadServlet",urlPatterns = "/updatehead")
 @MultipartConfig
 public class UpdateHeadServlet extends HttpServlet {
-    DatabaseConnection databaseConnection = new DatabaseConnection();
-    Connection connection=null;
-    PreparedStatement preparedStatement=null;
-
+    UserPageDao userPageDao=new UserPageDao();
 
     private String getFilename(Part part) {
         String contentDispositionHeader = part.getHeader("content-disposition");
@@ -50,44 +44,15 @@ public class UpdateHeadServlet extends HttpServlet {
         String belongid=request.getParameter("belongid");
         part.write(path);
 
-        try{
-            connection = databaseConnection.getDbConnection();
-            if (connection!=null){
-                String sql="update selfinf set head_img=? where belonguserid=?";
-                preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setString(1,path);
-                preparedStatement.setString(2,belongid);
-                preparedStatement.executeUpdate();
+        userPageDao.updateHead(belongid,path);
 
-                request.getRequestDispatcher("jsp/userpage.jsp").forward(request,response);
-//                response.sendRedirect("jsp/userpage.jsp");
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            try{
-                closeConnection();
-            }
-            catch (Exception e){
-
-            }
-        }
+        request.getRequestDispatcher("jsp/userpage.jsp").forward(request,response);
 
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
-    private void closeConnection(){
-        if(connection != null){
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            connection = null;
-        }
     }
 
 }
